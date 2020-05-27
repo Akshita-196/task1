2cae0953d5b367c34c8c2bce67b3063a3be9a1dc
Akshita-196 <66004587+Akshita-196@users.noreply.github.com>
2020-05-27 14:22:57 +0530

    create hello.py
    prints area of the circle and extension of filename

---
+++ hello.py
@@ -0,0 +1,13 @@
+# task1
+code-prints area of the circle
+import math
+r= float(input("input the radius of the circle:"))
+area=math.pi*r*r
+print("Area of the circle with radius r:",area)
+
+#task2
+code2- print the extension of file
+f= input("input the filename:")
+f_extns= f.split(".")
+print("the extension of the file is:" , repr(f_extns[-1]))
+
